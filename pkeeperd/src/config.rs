046440410808078cc95fff6a8b1116b0949becf3
Mattia Procopio <promat85@gmail.com>
2023-11-07 23:58:50 +0100

    Stub config data structs

--- pkeeperd/src/config.rs
+++ pkeeperd/src/config.rs
@@ -1,1 +1,29 @@
-struct Config {}
+pub struct Day {
+    name: String,
+    total_time: u16,
+    wake_up: String,
+    sleep: String,
+}
+
+pub struct User {
+    name: String,
+    schedule: Vec<Day>,
+}
+
+pub struct Config {
+    users: Vec<User>,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        // let raw_users = find a way to extract data from the database
+        let users: Vec<User> = Vec::new();
+        // for every user in raw_users;
+        //   create empty vec
+        //   create all days for a given schedule
+        //   pack all days into the vector created before
+        //   create a new User
+        //   users.push(user);
+        Config { users }
+    }
+}
