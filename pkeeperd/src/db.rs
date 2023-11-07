ab621c4c00ce0ee57b99a36163bb9901bfd1f2fa
Mattia Procopio <promat85@gmail.com>
2023-11-07 23:59:00 +0100

    Stub DB creation

--- pkeeperd/src/db.rs
+++ pkeeperd/src/db.rs
@@ -1,5 +1,6 @@
 pub fn init_db() {
     let connection = sqlite::open("test.db").unwrap();
-    let query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, config TEXT);";
+    let query =
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, schedule TEXT);";
     connection.execute(query).unwrap();
 }
